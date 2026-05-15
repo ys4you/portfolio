cddac1efa7e52a9ba23ce7b89f44592e18eaf83d
Yesse <116799248+ys4you@users.noreply.github.com>
2026-05-15 20:27:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "/portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
